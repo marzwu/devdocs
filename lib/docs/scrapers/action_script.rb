f8ce0bf4c1cdabf16346ae5268debbdbff1daf8f
marzwu <marzwu@gmail.com>
2015-11-15 11:13:48 +0800

    1.update ruby version from 2.2.2 to 2.2.3; 2.add doc actionscript3.0

---
+++ lib/docs/scrapers/action_script.rb
@@ -0,0 +1,40 @@
+module Docs
+  class ActionScript < UrlScraper
+    include StubRootPage
+
+    self.name = 'ActionScript 3.0'
+    self.slug = 'actionscript_3.0'
+    self.type = 'action_script'
+    self.version = '3.0'
+    self.base_url = "http://help.adobe.com/zh_CN/FlashPlatform/reference/action_script/3/"
+
+    html_filters.push 'action_script/clean_html', 'action_script/entries', 'title'
+    text_filters.push 'action_script/clean_urls'
+
+    options[:title] = false
+    options[:root_title] = 'ActionScript 3.0'
+
+    options[:fix_urls] = ->(url) do
+      url.sub! '/partials/api/api/', '/partials/api/'
+      url.sub! %r{/api/(.+?)/api/}, '/api/'
+      url.sub! %r{/partials/api/(.+?)(?<!\.html)(?:\z|(#.*))}, '/partials/api/\1.html\2'
+      url
+    end
+
+    options[:skip] = %w(ng.html)
+
+    options[:attribution] = <<-HTML
+      &copy; 2010&ndash;2015 Google, Inc.<br>
+      Licensed under the Creative Commons Attribution License 3.0.
+    HTML
+
+    private
+
+    def root_page_body
+      require 'capybara'
+      Capybara.current_driver = :selenium
+      Capybara.visit("https://code.angularjs.org/#{self.class.version}/docs/api")
+      Capybara.find('.side-navigation')['innerHTML']
+    end
+  end
+end
